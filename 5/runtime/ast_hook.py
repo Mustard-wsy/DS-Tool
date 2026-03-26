0a66a6f6ae9e787f92f6a7968f3241de9e59a61d
Mustard-wsy <241502017@smail.nju.edu.cn>
2026-03-26 16:56:13 +0800

    Switch v6 to explicit dsvis.auto() mode

---
+++ 5/runtime/ast_hook.py
@@ -0,0 +1,40 @@
+import ast
+import os
+
+from .injector import InjectTrigger
+
+PRINT_TRANSFORMED_CODE = True
+
+
+def run_file(filepath):
+    with open(filepath, "r", encoding="utf-8") as f:
+        code = f.read()
+
+    tree = ast.parse(code, filename=filepath)
+
+    transformer = InjectTrigger()
+    new_tree = transformer.visit(tree)
+    ast.fix_missing_locations(new_tree)
+
+    if PRINT_TRANSFORMED_CODE:
+        print(ast.unparse(new_tree))
+
+    compiled = compile(new_tree, filename=filepath, mode="exec")
+
+    from .trigger import trigger
+
+    previous_flag = os.environ.get("DSVIS_AST_RUNNING")
+    os.environ["DSVIS_AST_RUNNING"] = "1"
+
+    global_env = {
+        "trigger": trigger,
+        "__name__": "__ast_exec__",
+        "__file__": filepath,
+    }
+    try:
+        exec(compiled, global_env)
+    finally:
+        if previous_flag is None:
+            os.environ.pop("DSVIS_AST_RUNNING", None)
+        else:
+            os.environ["DSVIS_AST_RUNNING"] = previous_flag
